e95129c8bf07eae0804a945e98899f8547f86a24
Ari Benjamin <arisbenjamin@gmail.com>
2024-08-23 15:24:54 -0400

    remove social panel

--- strip_js.py
+++ strip_js.py
@@ -1,6 +1,27 @@
 import pathlib
 from tqdm import tqdm
+import os
+from bs4 import BeautifulSoup
 
+def strip_social(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+
+    # Find and remove the specific nav element
+    social_nav = soup.find('nav', {'id': 'nav-social'})
+    if social_nav:
+        social_nav.decompose()
+
+    # Write the modified HTML back to the file
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(str(soup))
+
+def process_all_html_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.html'):
+                filepath = os.path.join(root, file)
+                strip_social(filepath)
 
 def strip_js(filename, bad_lines):
     good_lines = []
@@ -14,7 +35,6 @@ def strip_js(filename, bad_lines):
     with open(filename, "w") as f:
         f.write(text)
 
-
 if __name__ == "__main__":
     # Go through _build/html, and remove all extraneous javascript. This is all
     # very silly, but upgrading sphinx-book-theme's version to reject the bad js
@@ -22,3 +42,6 @@ if __name__ == "__main__":
     files = pathlib.Path("_build/html").glob("*.html")
     for f in tqdm(files):
         strip_js(f, ["thebe.js"])
+
+    # Remove the social nav from all HTML files
+    process_all_html_files("_build/html")
